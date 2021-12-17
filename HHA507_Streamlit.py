580f534a3688919bac19126df2767d9e395c8909
Shu <shuwen.tan@stonybrook.edu>
2021-12-16 21:12:09 -0500

    Updated with answers to Q1-3

--- HHA507_Streamlit.py
+++ HHA507_Streamlit.py
@@ -11,7 +11,7 @@ def load_hospitals():
    hospital_df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv')
    return hospital_df
 @st.cache
-def load_inatpatient():
+def load_inpatient():
     inpatient_df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/inpatient_2015.csv')
     return inpatient_df
 @st.cache
@@ -37,7 +37,7 @@ st.write('6. ')
 
 ##Load the datasets:     
 hospital_df = load_hospitals()
-inpatient_df = load_inatpatient()
+inpatient_df = load_inpatient()
 outpatient_df = load_outpatient()
 
 ##Preview the datasets
@@ -61,3 +61,43 @@ NY = hospital_df[hospital_df['state'] == 'NY']
 st.header('Summary Info for Hospitals in New York')
 st.markdown('This dataset shows hospitals located in New York, filtered out from the main hospital dataframe, excluding SBU hospital')
 st.dataframe(NY)
+
+##Answering question 1.
+table1 = NY['hospital_overall_rating'].value_counts().reset_index()
+st.header('Q1. How does Stony Brook Univeristy Hospital compare to the rest of New York in overall rating?')
+st.subheader('Hospital rating for New York')
+st.markdown('From this table, we can determine that most hospitals in New York has a overall rating of 1, with 5 representing the highest rating.')
+st.markdown('To answer question 1, we know from the previous table that Stony Brook University Hospital has a overall rating of 4, thus Stony Brook University has a higher overall rating than most hospitals within New York. ')
+st.dataframe(table1)
+
+##Create dataframe unique for Stony Brook University Hospital Inpatient info for question 2
+SBU_inpatient = inpatient_df[inpatient_df['provider_id']==330393]
+st.header('Inpatient Data for Stony Brook')
+st.markdown('This shows inpatient data for Stony Brook University Hospital, which is filtered from the main inpatient dataset')
+st.dataframe(SBU_inpatient)
+
+##Question 2. Most expensive inpatient DRGs for SBU
+st.header('Q2. What is the most expensive inpatient DRGs code for Stony Brook University Hospital?')
+st.subheader('Stony Brook Inpatient DRGs Pivot Table')
+SBU_inpatient_DRGs_pivot = SBU_inpatient.pivot_table(index=['provider_id','provider_name','drg_definition'],values=['average_total_payments'])
+SBU_inpatient_DRGs_desc = SBU_inpatient_DRGs_pivot.sort_values(['average_total_payments'], ascending=False)
+st.markdown('DRGs is diagnostic related group, which is a classification system that stanrdardizes prospective payment to hospitals')
+st.markdown('With information listed on this table, we can determine that the most expensive inpatient DRGs code for Stony Brook Univeristy Hospital is 003, with an average total payments of $21,667.00')
+st.markdown('003 - ECMO OR TRACH W MV >96 HRS OR PDX EXC FACE, MOUTH & NECK W MAJ O.R.')
+st.dataframe(SBU_inpatient_DRGs_desc)
+
+##Create dataframe unique for Stony Brook University Hospital Outpatient info for question 3
+SBU_outpatient = outpatient_df[outpatient_df['provider_id']==330393]
+st.header('Outpatient Data for Stony Brook')
+st.markdown('This shows outpatient data for Stony Brook University Hospital, which is filtered from the main inpatient dataset')
+st.dataframe(SBU_outpatient)
+
+##Question 3. Most expensive inpatient APCs for SBU
+st.header('Q3. What is the most expensive inpatient APCs code for Stony Brook University Hospital?')
+st.subheader('Stony Brook Inpatient APCs Pivot Table')
+SBU_outpatient_DRGs_pivot = SBU_outpatient.pivot_table(index=['provider_id','provider_name','apc'],values=['average_total_payments'])
+SBU_outpatient_DRGs_desc = SBU_outpatient_DRGs_pivot.sort_values(['average_total_payments'], ascending=False)
+st.markdown('APCs is ambulatory payment classsifications, a classification system for outpatient services')
+st.markdown('With information listed on this table, we can determine that the most expensive outpatient APCs code for Stony Brook Univeristy Hospital is 0074, with an average total payments of $2,307.21')
+st.markdown('0074 - Level IV Endoscopy Upper Airway')
+st.dataframe(SBU_outpatient_DRGs_desc)
